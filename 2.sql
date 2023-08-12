ad5d207cf3dc64734af2f7f59faa9d65d8bd6938
NaniOdugu <142076162+NaniOdugu@users.noreply.github.com>
2023-08-12 11:17:18 +0530

    Add files via upload

---
+++ 2.sql
@@ -0,0 +1,7 @@
+/*2.Write a SELECT statement that lists the name, product number, and color of each product from the Production.Product table*/
+USE AdventureWorks2022;
+SELECT
+Name,
+ProductNumber,
+Color
+FROM Production.Product
